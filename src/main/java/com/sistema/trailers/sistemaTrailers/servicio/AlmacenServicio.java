ba15822d9014a160e8ad13f78b03a2c1dcdb0970
FerLedesma <ferledesma352@gmail.com>
2024-02-27 09:40:14 -0300

    Creacion Almacen Servicio

---
+++ src/main/java/com/sistema/trailers/sistemaTrailers/servicio/AlmacenServicio.java
@@ -0,0 +1,19 @@
+package com.sistema.trailers.sistemaTrailers.servicio;
+
+import jakarta.annotation.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+
+public interface AlmacenServicio {
+
+    public void iniciarAlmacenArchivos();
+
+    public String almacenarArchivo(MultipartFile archivo);
+
+    public Path cargarArchivo(String nombreArchivo);
+
+    public Resource cargarComoRecurso(String nombreArchivo);
+
+    public void eliminarArchivo(String nombreArhivo);
+}
