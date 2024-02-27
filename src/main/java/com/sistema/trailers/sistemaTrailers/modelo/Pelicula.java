6c09ea88698aa3a4055a3680e3ac66ce7b7009b3
FerLedesma <ferledesma352@gmail.com>
2024-02-27 09:32:16 -0300

    Creacion clase Pelicula

--- src/main/java/com/sistema/trailers/sistemaTrailers/modelo/Pelicula.java
+++ src/main/java/com/sistema/trailers/sistemaTrailers/modelo/Pelicula.java
@@ -1,6 +1,12 @@
 package com.sistema.trailers.sistemaTrailers.modelo;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 public class Pelicula {
@@ -9,7 +15,23 @@ public class Pelicula {
     @Column(name = "id_pelicula")
     private Integer id;
 
+    @NotBlank
     private String titulo;
 
+    @NotBlank
+    private String sinopsis;
+    @NotBlank
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate fechaEstreno;
+
+    @NotBlank
+    private  String youtubeTrailerId;
+
+    private String rutaPortada;
+
+    @NotEmpty
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "genero_pelicula",joinColumns = @JoinColumn(name = "id_pelicula"),inverseJoinColumns = @JoinColumn(name = "id_genero"))
+    private List<Genero> generos;
 
 }
